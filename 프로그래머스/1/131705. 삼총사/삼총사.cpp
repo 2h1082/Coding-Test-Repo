34746cb5629999224379b6b56a0d92700de84adc
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-01-01 23:50:57 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    for(int i=0;i<number.size()-2;i++){
+        for(int j=i+1;j<number.size()-1;j++){
+            for(int k=j+1;k<number.size();k++){
+                int sum=number.at(i)+number.at(j)+number.at(k);
+                if(sum==0){
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer;
+}
