17b9c0f064dd9206d57bd259ff99d6ac53385689
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-03-31 21:24:37 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 25.22 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<vector<int>> users, vector<int> emoticons) {
+    vector<int> answer(2,0);
+    vector<vector<int>> price(7,vector<int>(2,0));
+    int n=emoticons.size();
+    function<void(int)> DFS=[&](int index)
+    {
+        if(index<n)
+        {
+            for(int i=10;i<=40;i+=10)
+            {
+                price[index][0]=(100-i)*emoticons[index]/100;
+                price[index][1]=i;
+                DFS(index+1);
+            }
+        }
+        else
+        {
+            vector<int> cur(2,0);
+            for(int i=0;i<users.size();i++)
+            {
+                int Total=0;
+                for(int j=0;j<index;j++)
+                {
+                    if(price[j][1]>=users[i][0])
+                    {
+                        Total+=price[j][0];
+                    }
+                }
+                if(Total>=users[i][1])
+                {
+                    cur[0]++;
+                }
+                else
+                {
+                    cur[1]+=Total;
+                }
+            }
+            if(cur[0]>answer[0]||(cur[0]==answer[0]&&cur[1]>answer[1]))
+            {
+                answer=cur;
+            }
+            return;
+        }        
+    };
+    DFS(0);
+    return answer;
+}
