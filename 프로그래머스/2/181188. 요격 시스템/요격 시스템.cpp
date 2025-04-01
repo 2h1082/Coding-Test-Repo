6c848d82369605e758505d8d41433b2162ce455b
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-04-01 16:51:50 +0900

    [level 2] Title: 요격 시스템, Time: 141.23 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -2,26 +2,23 @@
 
 using namespace std;
 
+
 int solution(vector<vector<int>> targets) {
     int answer = 0;
-    sort(targets.begin(),targets.end());
-    int s=targets[0][0];
-    int e=targets[0][1];
+    sort(targets.begin(),targets.end(),[](vector<int>& a, vector<int>& b){
+        return a[1]<b[1];
+    });
     
-    for(int i=1;i<targets.size();i++)
+    int e=-1;
+    
+    for(int i=0;i<targets.size();i++)
     {
+        
         if(e<=targets[i][0])
         {
             answer++;
-            s=targets[i][0];
             e=targets[i][1];
-        }
-        else
-        {
-           s=targets[i][0];
-           e= e>targets[i][1]?targets[i][1]:e; 
-        }
-        
+        }        
     }
-    return answer+1;
+    return answer;
 }
