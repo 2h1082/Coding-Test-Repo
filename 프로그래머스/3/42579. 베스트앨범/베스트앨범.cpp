b106d12b28c70113e0a651f0a615d587a62fff04
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-04-11 23:00:04 +0900

    [level 3] Title: 베스트앨범, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -5,11 +5,11 @@ using namespace std;
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
     map<string,int> PlayCount;
-    multimap<string,pair<int,int>> CountIdx;
+    map<string,vector<pair<int,int>>>CountIdx;
     for(int i=0;i<genres.size();i++)
     {
         PlayCount[genres[i]]+=plays[i];
-        CountIdx.insert({genres[i],{plays[i],i}});
+        CountIdx[genres[i]].push_back({plays[i],i});
     }
     vector<pair<string,int>> S(PlayCount.begin(),PlayCount.end());
     sort(S.begin(),S.end(),[&](auto& a,auto& b){
@@ -19,19 +19,13 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     {
         int cnt=0;
         string g=it->first;
-        vector<pair<int, int>> songList;
-        auto range=CountIdx.equal_range(g);
-        for(auto i=range.first;i!=range.second;i++)
-        {
-            songList.push_back(i->second);
-        }
-        sort(songList.begin(),songList.end(),[&](auto& a, auto& b){
+        sort(CountIdx[g].begin(),CountIdx[g].end(),[&](auto& a, auto& b){
             if(a.first==b.first) return a.second<b.second;
             return a.first>b.first;
         });
-        for(int i=0;i<songList.size()&&i<2;i++)
+        for(int i=0;i<CountIdx[g].size()&&i<2;i++)
         {
-            answer.push_back(songList[i].second);
+            answer.push_back(CountIdx[g][i].second);
         }
     }
     return answer;
