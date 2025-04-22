cae5b06b5efe39c970f6e6b8d0af42003f2829eb
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-04-22 23:42:33 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 8 ms, Memory: 2204 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -4,45 +4,47 @@
 
 using namespace std;
 
-const int MAX = 500001;
-
-int main() {
+const int MAX=500001;
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+    cout.tie(NULL);
+    
     int n, k;
     cin >> n >> k;
-
-    // visited[짝수/홀수][위치]
-    vector<vector<bool>> visited(2, vector<bool>(MAX, false));
-    queue<pair<int, int>> q;  // {위치, 시간}
-    q.push({n, 0});
+    
+    vector<vector<bool>> visited(2,vector<bool>(MAX,false));
+    queue<int> q;
+    q.push(n);
     visited[0][n] = true;
-
-    int time = 0;
-    while (!q.empty()) {
-        int brother = k + time * (time + 1) / 2;
-        if (brother >= MAX) break;
-        if (visited[time % 2][brother]) {
-            cout << time << '\n';
+    int time=0;
+    while (!q.empty())
+    {
+        int brother=k+time*(time+1)/2;
+        if (brother>=MAX) break;
+        if (visited[time%2][brother])
+        {
+            cout<<time<<"\n";
             return 0;
         }
-
-        int size = q.size();
+        int size=q.size();
         time++;
-        for (int i = 0; i < size; ++i) {
-            int cur = q.front().first;
-            int t = q.front().second;
+        for (int i=0;i<size;i++)
+        {
+            int cur=q.front();
             q.pop();
-
-            for (int next : {cur - 1, cur + 1, cur * 2}) {
-                if (next >= 0 && next < MAX && !visited[time % 2][next]) {
-                    visited[time % 2][next] = true;
-                    q.push({next, time});
+            for (int next:{cur-1,cur+1,cur*2})
+            {
+                if (next>=0&&next<MAX&&!visited[time%2][next])
+                {
+                    visited[time%2][next]=true;
+                    q.push(next);
                 }
             }
         }
     }
-    cout << -1 << '\n';
+    
+    cout << "-1\n"; // 따라잡을 수 없는 경우
     return 0;
 }
