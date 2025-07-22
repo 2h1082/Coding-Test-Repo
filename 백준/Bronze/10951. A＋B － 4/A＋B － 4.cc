6c4a581340e5296d89a7a5b7658c52c37d5d2eca
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-07-22 21:39:31 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    while(1)
+    {
+        int A=0, B=0;
+        cin>>A>>B;
+        if(A==0 || B==0) break;
+        cout<<A+B<<"\n";
+    }
+}
