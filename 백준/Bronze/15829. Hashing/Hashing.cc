ab6bcef8ba8a6e6dcb520a4c81ca07d9b3d9c5ff
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-07-29 00:03:26 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -11,7 +11,7 @@ int main()
     for(int i=0;i<L;++i)
     {
         int Alpha=Text[i]-'a'+1;
-        Hash+=Alpha*R;
+        Hash=(Hash+Alpha*R)%1234567891;
         R=R*31%1234567891;
     }
     cout<<Hash;
