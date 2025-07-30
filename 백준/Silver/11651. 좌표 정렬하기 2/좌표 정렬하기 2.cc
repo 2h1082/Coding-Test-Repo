651149d1a978ef6b696f45191f2cc2570f45ca05
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-07-31 02:25:04 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 56 ms, Memory: 7536 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N=0;
+    cin>>N;
+    
+    vector<vector<int>> Point(N,vector<int>(2));
+    for(int i=0;i<N;++i)
+    {
+        cin>>Point[i][0]>>Point[i][1];
+    }
+    sort(Point.begin(),Point.end(),[&](const vector<int>& a, const vector<int>& b){
+        if(a[1]!=b[1]) return a[1]<b[1];
+        return a[0]<b[0];
+    });
+    for(auto& P : Point)
+    {
+        cout<<P[0]<<" "<<P[1]<<"\n";
+    }
+}
