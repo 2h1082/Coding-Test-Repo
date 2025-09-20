c302680b63c160d8f456eeeffd3d8ca199b8bc49
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-09-20 22:50:49 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int N=0,M=0;
+vector<int> Nums;
+void DFS(int Index, int Count)
+{
+    if(Count==M)
+    {
+        for(auto& Num : Nums)
+        {
+            cout<<Num<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+    for(int i=Index;i<=N;++i)
+    {
+        Nums.push_back(i);
+        DFS(i,Count+1);
+        Nums.pop_back();
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin>>N>>M;
+    Nums.reserve(M);
+    DFS(1,0);
+}
