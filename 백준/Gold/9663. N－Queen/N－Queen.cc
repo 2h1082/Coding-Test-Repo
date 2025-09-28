153e824f9d4bf26feb936b42eede793bafefa95d
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-09-28 21:36:46 +0900

    [Gold IV] Title: N-Queen, Time: 1996 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -3,50 +3,34 @@
 
 using namespace std;
 int N=0, Solution=0;
-vector<vector<bool>> Board;
-int dx[3]={0,-1,1};
-int dy[3]={-1,-1,-1};
-bool IsPossible(int X, int Y)
+vector<bool> Col, Diag1, Diag2;
+
+void NQueen(int Y)
 {
-    for(int j=0;j<Y;++j)
-    {
-        if(Board[j][X]) return false;
-        int Diff=Y-j;
-        if(X-Diff>=0&&Board[j][X-Diff])
-        {
-            return false;
-        }
-        if(X+Diff<N&&Board[j][X+Diff])
-        {
-            return false;
-        }
-    }
-    return true;
-}
-void NQueen(int Y, int Count)
-{
-    if(Count==N)
+    if(Y==N)
     {
         Solution++;
         return;
     }
-    if(Y>=N) return;
     for (int i=0;i<N;++i)
     {
-        if (IsPossible(i,Y))
-        {
-            Board[Y][i]=true;
-            NQueen(Y+1,Count+1);
-            Board[Y][i]=false;
-        }
+        if(Col[i]||Diag1[Y-i+N-1]||Diag2[Y+i]) continue;
+        Col[i]=true;
+        Diag1[Y-i+N-1]=true;
+        Diag2[Y+i]=true;
+        NQueen(Y+1);
+        Col[i]=false;
+        Diag1[Y-i+N-1]=false;
+        Diag2[Y+i]=false;
     }
 }
 int main()
 {
     cin>>N;
-    Board.assign(N,vector<bool>(N,false));
+    Col.assign(N,false);
+    Diag1.assign(2*N-1,false);
+    Diag2.assign(2*N-1,false);
     
-    
-    NQueen(0,0);
+    NQueen(0);
     cout<<Solution;
 }
