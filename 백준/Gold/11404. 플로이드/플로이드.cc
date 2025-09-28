82fec0bc3f2899f12091a041b3647037b1b9be95
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-09-29 08:42:11 +0900

    [Gold IV] Title: 플로이드, Time: 84 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+int main()
+{
+    int N=0, M=0;
+    cin>>N>>M;
+    vector<vector<int>> Bus(N,vector<int>(N,INT_MAX));
+    for(int i=0;i<N;++i)
+    {
+        Bus[i][i]=0;
+    }
+    for(int i=0;i<M;++i)
+    {
+        int From,To,Cost;
+        cin>>From>>To>>Cost;
+        Bus[From-1][To-1]=min(Bus[From-1][To-1],Cost);
+    }
+    for(int k=0;k<N;++k)
+    {
+        for(int i=0;i<N;++i)
+        {
+            for(int j=0;j<N;++j)
+            {
+                if(Bus[i][k]!=INT_MAX&&Bus[k][j]!=INT_MAX)
+                {
+                    Bus[i][j]= min(Bus[i][j],Bus[i][k]+Bus[k][j]);
+                }
+            }
+        }
+    }
+    
+    for(int i=0;i<N;++i)
+    {
+        for(int j=0;j<N;++j)
+        {
+            if (Bus[i][j]==INT_MAX)
+            {
+                cout<<"0 ";
+            }
+            else
+            {
+                cout<<Bus[i][j]<<" ";
+            }
+        }
+        cout<<"\n";
+    }
+}
