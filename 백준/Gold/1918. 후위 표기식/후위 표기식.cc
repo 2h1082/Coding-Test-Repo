850c981dc0d9fa9a06dfdef6ff4b5588ef71b015
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-10-03 09:25:49 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int GetPriority(char& A)
+{
+    if(A=='+'||A=='-')
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+int main()
+{
+    string Input;
+    cin>>Input;
+    string Formula, Stack;
+    bool bShouldPop=false;
+    for(int i=0;i<Input.size();++i)
+    {
+        if(Input[i]>='A'&&Input[i]<='Z')
+        {
+            Formula+=Input[i];
+        }
+        else if (Input[i]=='(')
+        {
+            Stack+=Input[i];
+        }
+        else if(Input[i]==')')
+        {
+            while(!Stack.empty())
+            {
+                if(Stack.back()=='(')
+                {
+                    Stack.pop_back();
+                    break;
+                }
+                Formula+=Stack.back();
+                Stack.pop_back();
+            }
+        }
+        else if(Input[i]=='*'||Input[i]=='/'||Input[i]=='+'||Input[i]=='-')
+        {
+            while(!Stack.empty()&&Stack.back()!='('
+                 && GetPriority(Stack.back())>=GetPriority(Input[i]))
+            {
+                Formula+=Stack.back();
+                Stack.pop_back();
+            }
+            Stack+=Input[i];
+        }
+    }
+    while(!Stack.empty())
+    {
+        Formula+=Stack.back();
+        Stack.resize(Stack.size()-1);
+    }
+    cout<<Formula;
+}
