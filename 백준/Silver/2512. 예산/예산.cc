5040f8221be0a61fe1b3cbc502efaffc277abde4
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-11-30 20:53:36 +0900

    [Silver II] Title: 예산, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.cc
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int N=0, M=0;
+bool IsPossible(vector<int>& Num, int Max)
+{
+    int Sum=0;
+    for (auto n : Num)
+    {
+        Sum+=min(Max,n);
+        if(Sum>M) return false;
+    }
+    return Sum<=M;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin>>N;
+    vector<int> Num(N,0);
+    int MaxVal=0;
+    for(auto& n : Num)
+    {
+        cin>>n;
+        MaxVal=max(MaxVal,n);
+    }
+    cin>>M;
+    
+    int L=0, R=MaxVal;
+    while (L<R)
+    {
+        int Mid = L + (R - L + 1) / 2;   
+        if (IsPossible(Num,Mid))     L=Mid;
+        else                             R=Mid-1;
+    }
+    cout<<L;
+}
