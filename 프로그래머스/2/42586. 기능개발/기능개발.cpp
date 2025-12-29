65de4b86cebffe87c47d2844282a9cdf9f3bcf73
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-12-29 16:08:04 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,27 +1,23 @@
-#include <string>
 #include <vector>
-
 using namespace std;
 
-vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    vector<int> answer;
-    vector<int> period(speeds.size());
-    for(int i=0;i<speeds.size();++i)
+vector<int> solution(vector<int> Progresses, vector<int> Speeds) 
+{
+    vector<int> Ans;
+    int N=Speeds.size(), Max=0;
+    
+    for(int i=0;i<N;++i)
     {
-        period[i]=(100-progresses[i]+speeds[i]-1)/speeds[i];
-    }
-    int count=1;
-    int prevPeriod=period[0];
-    for(int j=1;j<speeds.size();++j)
-    {
-        if(prevPeriod<period[j])
+        int Cur=(99-Progresses[i])/Speeds[i] + 1;
+        if(Ans.empty() || Cur > Max)
         {
-            answer.push_back(count);
-            prevPeriod=period[j];
-            count=0;
+            Ans.push_back(1);
+            Max=max(Cur,Max);
         }
-        ++count;
+        else
+        {
+            Ans.back()++;
+        }
     }
-    answer.push_back(count);
-    return answer;
+    return Ans;
 }
