941deab855de9418372c7bb60126ed05998d725b
2h1082 <95967939+2h1082@users.noreply.github.com>
2025-12-30 21:04:28 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.14 ms, Memory: 3.74 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -1,38 +1,36 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int solution(vector<vector<int>> jobs) {
-    int answer = 0;
-    int n=jobs.size();
-    priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
-    for(int i=0;i<n;i++)
+int solution(vector<vector<int>> Jobs) 
+{
+    int Ans=0, N=Jobs.size();
+    
+    sort(Jobs.begin(),Jobs.end());
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> Pq;
+    
+    int Time=0, Idx=0, Cnt=0;
+    while(Cnt < N)
     {
-        jobs[i].push_back(i);
-    }
-    sort(jobs.begin(),jobs.end());
-    int time=jobs[0][0];
-    int next=1;
-    pq.push({jobs[0][1],jobs[0][0],jobs[0][2]});
-    while(next<n||!pq.empty())
-    {
-        while((next<n)&&(time>=jobs[next][0])) 
+        while(Idx < N && Jobs[Idx][0] <= Time) 
         {
-            pq.push({jobs[next][1],jobs[next][0],jobs[next][2]});
-            next++;
-        }
-        if(pq.empty())
-        {
-            time=jobs[next][0];
-            pq.push({jobs[next][1],jobs[next][0],jobs[next][2]});
-            next++;
+            Pq.push({Jobs[Idx][1],Jobs[Idx][0]});
+            Idx++;
         }
         
-        auto v=pq.top(); //0:len, 1:start time, 2:index
-        pq.pop();
-        time+=v[0];
-        answer+=time-v[1];
+        if(!Pq.empty())
+        {
+            auto [Cost, InTime]=Pq.top();
+            Pq.pop();
+            
+            Time+=Cost;
+            Ans+=Time-InTime;
+            Cnt++;
+        }
+        else
+        {
+            Time=Jobs[Idx][0];
+        }
     }
     
-    return answer/n;
+    return Ans/N;
 }
