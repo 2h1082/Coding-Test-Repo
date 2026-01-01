cfeca08d81cf9c076c04ebea9e389e22dcf01644
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-01-01 18:01:49 +0900

    [level 1] Title: 모의고사, Time: 0.07 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,23 +1,21 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include "bits/stdc++.h"
 using namespace std;
 
-vector<int> solution(vector<int> answers) {
-    vector<int> answer;
-    vector<int> num1={1,2,3,4,5};
-    vector<int> num2={2,1,2,3,2,4,2,5};
-    vector<int> num3={3,3,1,1,2,2,4,4,5,5};
-    int sol1=0, sol2=0, sol3=0;
-    for(int i=0;i<answers.size();i++){
-        if(num1[i%5]==answers[i]) sol1++;
-        if(num2[i%8]==answers[i]) sol2++;
-        if(num3[i%10]==answers[i]) sol3++;
+vector<int> solution(vector<int> Answers) 
+{
+    vector<int> Ans;
+    vector<int> P1={1,2,3,4,5}, P2={2,1,2,3,2,4,2,5}, P3={3,3,1,1,2,2,4,4,5,5};
+    int Sol1=0, Sol2=0, Sol3=0, N=Answers.size();
+    for(int i=0;i<N;++i)
+    {
+        if(Answers[i]==P1[i%5])  ++Sol1;
+        if(Answers[i]==P2[i%8])  ++Sol2;
+        if(Answers[i]==P3[i%10]) ++Sol3;
     }
-    int Max=max({sol1,sol2,sol3});
-    if (sol1==Max) answer.push_back(1);
-    if (sol2==Max) answer.push_back(2);
-    if (sol3==Max) answer.push_back(3);
-    return answer;
+    int Max=max({Sol1,Sol2,Sol3});
+    if(Max==Sol1) Ans.push_back(1);
+    if(Max==Sol2) Ans.push_back(2);
+    if(Max==Sol3) Ans.push_back(3);
+    
+    return Ans;
 }
