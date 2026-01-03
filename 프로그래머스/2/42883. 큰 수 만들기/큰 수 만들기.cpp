53162c2f34da739934e9b794e8d5c7ae6b6a985b
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-01-04 08:50:21 +0900

    [level 2] Title: 큰 수 만들기, Time: 2350.20 ms, Memory: 5.34 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,27 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-string solution(string number, int k) {
-    string answer = "";
-    stack<char> s;
-    int toRemove = k;  
-
-    for (char digit : number) {
-        while (!s.empty() && toRemove > 0 && s.top() < digit) {
-            s.pop();
-            toRemove--;
+string solution(string Number, int K) 
+{
+    string Ans = "";
+    stack<char> St;
+    int Cnt=K; // 삭제해야할 개수
+    
+    for(char& c : Number)
+    {
+        // 숫자를 하나씩 추가하면서 스택에 저장된 이전 인덱스의 수들이 현재 추가되는 수 미만이면 삭제
+        // - 스택에는 큰수부터 내림차순으로 수가 보존
+        while(!St.empty() && Cnt > 0 && St.top() < c)
+        {
+            St.pop();
+            --Cnt;
         }
-        s.push(digit);
+        St.push(c);
     }
-    while (toRemove > 0) {
-        s.pop();
-        toRemove--;
+    // 아직 지워야할 개수가 남았다면 나머지 삭제 
+    // - 내림차순으로 존재하니 위부터 삭제하면 적절
+    while(!St.empty() && Cnt > 0)
+    {
+        St.pop();
+        --Cnt;
     }
-
-    while (!s.empty()) {
-        answer = s.top()+answer;
-        s.pop();
+    while(!St.empty())
+    {
+        Ans=St.top()+Ans;
+        St.pop();
     }
-    return answer;
+    
+    return Ans;
 }
