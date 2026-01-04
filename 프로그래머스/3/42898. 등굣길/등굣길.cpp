80d80ce6069bf812f6621b83ad3eb2b975945a9a
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-01-05 06:59:21 +0900

    [level 3] Title: 등굣길, Time: 0.10 ms, Memory: 3.84 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -1,42 +1,37 @@
 #include "bits/stdc++.h"
-
 using namespace std;
 
 int Mod=1000000007;
-int solution(int M, int N, vector<vector<int>> P) {
-    int Ans = 0;
-    vector<vector<bool>> Wet(N,vector<bool>(M,false));
+int solution(int M, int N, vector<vector<int>> P) 
+{
     vector<vector<int>> Dp(N,vector<int>(M,0));
     
-    for(auto& R: P)
-    {
-        int x=R[0]-1 ,y=R[1]-1;
-        Wet[y][x]=true;
-    }
+    // 초기 맵 상태 설정
+    Dp[0][0]=1;
+    for(auto& Cur : P) Dp[Cur[1]-1][Cur[0]-1]=-1;
     
-    Dp[0][0]=0;
     for(int i=1;i<N;++i)
     {
-        if(Wet[i][0]) break;;
-        Dp[i][0]=1;
+        if(Dp[i][0]==-1) continue;
+        Dp[i][0]=Dp[i-1][0];
     }
-    for(int j=1;j<M;++j) 
+    for(int i=1;i<M;++i)
     {
-        if(Wet[0][j]) break;;
-        Dp[0][j]=1;
+        if(Dp[0][i]==-1) continue;
+        Dp[0][i]=Dp[0][i-1];
     }
     
-    
+    // Dp 갱신
     for(int i=1;i<N;++i)
     {
         for(int j=1;j<M;++j)
         {
-            if(Wet[i][j]) continue;
-            if(!Wet[i-1][j]) Dp[i][j]+=Dp[i-1][j];
-            if(!Wet[i][j-1]) Dp[i][j]+=Dp[i][j-1];
+            if(Dp[i][j]==-1) continue;
+            
+            if(Dp[i-1][j]>0) Dp[i][j]+=Dp[i-1][j];
+            if(Dp[i][j-1]>0) Dp[i][j]+=Dp[i][j-1];
             Dp[i][j]%=Mod;
         }
     }
-    Ans=Dp[N-1][M-1];
-    return Ans;
+    return Dp[N-1][M-1];
 }
