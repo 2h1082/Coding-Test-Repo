63742c9dbfbe46b9a05e570a9a75b09cc52d97c3
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-01-09 01:23:57 +0900

    [level 4] Title: 징검다리, Time: 5.48 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.cpp
+++ 프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -1,33 +1,25 @@
 #include "bits/stdc++.h"
-
 using namespace std;
 
 int solution(int Dist, vector<int> Rocks, int N) 
 {
-    int Ans=0;
     sort(Rocks.begin(),Rocks.end());
-    
-    int L=1, R=Dist;
-    while(L<=R)
+    int L=0, R=Dist;
+    while(L<R)
     {
-        int M=(L+R)/2;
+        int M=(L+R+1)/2;
         int Cnt=0, Prev=0;
-        for(int i=0;i<Rocks.size();++i)
-        {
-            if(Rocks[i] - Prev < M) ++Cnt;
-            else                    Prev=Rocks[i];
-        }
-        if(Dist - Prev < M) ++Cnt;
         
-        if(Cnt <= N)
+        for(auto& r : Rocks)
         {
-            Ans=M;
-            L=M+1;
+            if(r-Prev < M) Cnt++;
+            else           Prev=r;
         }
-        else
-        {
-            R=M-1;
-        }
+        if(Dist-Prev < M)  Cnt++;
+        
+        if(Cnt > N) R=M-1;
+        else        L=M;
     }
-    return Ans;
+    
+    return L;
 }
