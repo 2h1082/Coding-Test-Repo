69b6094a62996226a4b7dea994294529831a94ae
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-01-10 04:23:27 +0900

    [level 5] Title: 방의 개수, Time: 160.61 ms, Memory: 26.7 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.cpp
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.cpp
@@ -6,25 +6,25 @@ int Dy[]={1,1,0,-1,-1,-1,0,1};
 int solution(vector<int> Arrows) 
 {
     int Ans = 0;
-    map<pair<int,int>, bool> UsedNode;
-    map<tuple<int,int,int,int>, bool> UsedEdge;
+    
+    // 노드 및 간선 사용 여부 체크용 컨테이너
+    map<pair<int,int>,bool> UsedNode;
+    map<tuple<int,int,int,int>,bool> UsedEdge;
     UsedNode[{0,0}]=true;
     int Cx=0, Cy=0;
-    for(auto& A : Arrows)
+    for(auto& D : Arrows)
     {
-        for(int i=0;i<2;++i)
+        for(int i=0;i<2;++i) // 좌표 2배 확대하여 사용 (그래야 정확한 방 생성 여부 확인 가능)
         {
-            int Ny=Cy+Dy[A], Nx=Cx+Dx[A];
-            if(UsedNode[{Ny,Nx}] && !UsedEdge[{Cy,Cx,Ny,Nx}] && !UsedEdge[{Ny,Nx,Cy,Cx}])
-            {
-                Ans++;
-            }
+            int Ny=Cy+Dy[D], Nx=Cx+Dx[D];
+            // 방문했던 좌표인데 현재 이용한 간선은 이용되지 않았다면 방 생성 조건 충족
+            if(UsedNode[{Ny,Nx}] && !UsedEdge[{Ny,Nx,Cy,Cx}] && !UsedEdge[{Cy,Cx,Ny,Nx}]) ++Ans;
             
-            UsedNode[{Ny,Nx}]=true;
-            UsedEdge[{Cy,Cx,Ny,Nx}]=UsedEdge[{Ny,Nx,Cy,Cx}]=true;
+            UsedNode[{Ny,Nx}]=UsedEdge[{Ny,Nx,Cy,Cx}]=UsedEdge[{Cy,Cx,Ny,Nx}]=true; 
             Cy=Ny;
             Cx=Nx;
         }
     }
+    
     return Ans;
 }
