1a9e083b9b6e2722747b029c83170c83b6d836e4
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-01-14 12:44:51 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 36 ms, Memory: 2940 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -1,34 +1,36 @@
-#include <bits/stdc++.h>
-
+#include "bits/stdc++.h"
 using namespace std;
 using ll=long long;
-int N=0;
-vector<int> Nums;
+
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     while(1)
     {
+        int N;
         cin>>N;
         if(!N) break;
-        Nums.assign(N+1,0);
-        for(int i=0;i<N;++i)
-        {
-            cin>>Nums[i];
-        }
-        Nums[N]=0;
-        stack<int> st;
-        ll MaxArea=0;
+        
+        vector<int> H(N+1,0);
+        stack<int> St;
+        ll Max=0;
+        for(int i=0;i<N;++i) cin>>H[i];
         for(int i=0;i<=N;++i)
         {
-            while(!st.empty()&&Nums[st.top()]>Nums[i])
+            while(!St.empty() && H[St.top()] > H[i])
             {
-                int Height=Nums[st.top()];
-                st.pop();
-                int Width= st.empty() ? i : (i-st.top()-1);
-                MaxArea=max(MaxArea,1LL*Height*Width);
+                int Height=H[St.top()];
+                St.pop();
+                int Width= St.empty() ? i : i-St.top()-1; 
+                // 스택의 탑을 기준으로 이전 탑의 바로 앞까지 포함하여 면적 계산
+                // - (St.top()-1) ~ (i-1) 
+                // -> i-1 - (St.top()+1) + 1 = i-St.top()-1
+                Max=max(Max,1LL*Height*Width);
             }
-            st.push(i);
+            St.push(i);
         }
-        cout<<MaxArea<<"\n";
+        cout<<Max<<'\n';
     }
 }
