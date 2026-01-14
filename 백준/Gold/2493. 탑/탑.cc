65fa4a2865324e67897fd2cf95dce5ac85d59697
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-01-14 11:53:32 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 8072 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -5,23 +5,19 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
     
     int N;
     cin>>N;
     
-    vector<int> T(N,0);
-    stack<pair<int,int>> st;
+    stack<int> St;
+    vector<int> H(N,0), Ans(N,0);
     for(int i=0;i<N;++i)
     {
-        cin>>T[i];
+        cin>>H[i];
         
-        while(!st.empty() && st.top().first <= T[i]) st.pop();
-        
-        if(!st.empty()) cout<<st.top().second<<' ';
-        else            cout<<0<<' ';
-        st.push({T[i],i+1});
+        while(!St.empty() && H[St.top()] < H[i]) St.pop();
+        if(!St.empty()) Ans[i]=St.top()+1;
+        St.push(i);
     }
-    
-    
+    for(auto& a : Ans) cout<<a<<' ';
 }
