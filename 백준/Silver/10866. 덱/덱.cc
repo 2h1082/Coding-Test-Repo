3653ffc6a8e87fd038b44b85337c601d0bc9b502
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-01-16 12:51:16 +0900

    [Silver IV] Title: 덱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cc
@@ -0,0 +1,68 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N;
+    cin>>N;
+    
+    deque<int> Q;
+    while(N--)
+    {
+        string S;
+        cin>>S;
+        
+        if(S=="push_front")
+        {
+            int X;
+            cin>>X;
+            Q.push_front(X);
+        }
+        else if(S=="push_back")
+        {
+            int X;
+            cin>>X;
+            Q.push_back(X);
+        }
+        else if(S=="pop_front")
+        {
+            if(Q.empty()) cout<<-1<<'\n';
+            else
+            {
+                cout<<Q.front()<<'\n';
+                Q.pop_front();
+            }
+        }
+        else if(S=="pop_back")
+        {
+            if(Q.empty()) cout<<-1<<'\n';
+            else
+            {
+                cout<<Q.back()<<'\n';
+                Q.pop_back();
+            }
+        }
+        else if(S=="size")
+        {
+            cout<<Q.size()<<'\n';
+        }
+        else if(S=="empty")
+        {
+            if(Q.empty()) cout<<1<<'\n';
+            else          cout<<0<<'\n';
+        }
+        else if(S=="front")
+        {
+            if(Q.empty()) cout<<-1<<'\n';
+            else          cout<<Q.front()<<'\n';
+        }
+        else if(S=="back")
+        {
+            if(Q.empty()) cout<<-1<<'\n';
+            else          cout<<Q.back()<<'\n';
+        }
+    }
+}
