8d301a3bc0853289fea1921bc7f06c015e29ade7
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-01-20 00:00:37 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,66 +1,50 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
+#include "bits/stdc++.h"
 using namespace std;
 
-int Dx[4]={-1,1,0,0};
-int Dy[4]={0,0,-1,1};
-int BFS(int M, int N, vector<vector<int>> Grid)
+int dx[4]={0,0,-1,1};
+int dy[4]={-1,1,0,0};
+int main()
 {
-    int Count=0;
-    for(int i=0;i<M;++i)
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int T;
+    cin>>T;
+    
+    while(T--)
     {
-        for(int j=0;j<N;++j)
+        int M,N,K;
+        cin>>M>>N>>K;
+        vector<vector<int>> Map(N,vector<int>(M,0));
+        vector<pair<int,int>> Cand;
+        while(K--)
         {
-            if(Grid[i][j]==1)
+            int X,Y;
+            cin>>X>>Y;
+            Map[Y][X]=1;
+            Cand.push_back({Y,X});
+        }
+        int Cnt=0;
+        for(auto& [Sy, Sx]:Cand)
+        {
+            if(!Map[Sy][Sx]) continue;
+            ++Cnt;
+            queue<pair<int,int>> Q;
+            Q.push({Sy,Sx});
+            Map[Sy][Sx]=0;
+            while(!Q.empty())
             {
-                Count++;
-                queue<pair<int,int>> Q;
-                Q.push({i,j});
-                while(!Q.empty())
+                auto [Cy,Cx]=Q.front();
+                Q.pop();
+                for(int i=0;i<4;++i)
                 {
-                    int X=Q.front().first;
-                    int Y=Q.front().second;
-                    Q.pop();
-                    for(int k=0;k<4;++k)
-                    {
-                        int Nx=X+Dx[k];
-                        int Ny=Y+Dy[k];
-                        if(Nx>=0&&Nx<M&&Ny>=0&&Ny<N)
-                        {
-                            if(Grid[Nx][Ny]==1) 
-                            {
-                                Grid[Nx][Ny]=0;
-                                Q.push({Nx,Ny});
-                            }
-                        }
-                    }
+                    int Ny=Cy+dy[i], Nx=Cx+dx[i];
+                    if(Ny<0 || Ny>=N || Nx<0 || Nx>=M || !Map[Ny][Nx]) continue;
+                    Q.push({Ny,Nx});
+                    Map[Ny][Nx]=0;
                 }
             }
         }
-    }
-    return Count;
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int T=0;
-    cin>>T;
-    while(T--)
-    {
-        int M=0, N=0, K=0;
-        cin>>M>>N>>K;
-        vector<vector<int>> Grid(M,vector<int>(N,0));
-        for(int i=0;i<K;++i)
-        {
-            int X=0,Y=0;
-            cin>>X>>Y;
-            Grid[X][Y]=1;
-        }
-        int Answer=BFS(M,N,Grid);
-        cout<<Answer<<"\n";
+        cout<<Cnt<<'\n';
     }
 }
