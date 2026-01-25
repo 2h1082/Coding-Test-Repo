d081d802b872d252448c3a698696f40c03d047a2
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-01-26 00:53:01 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 92 ms, Memory: 8892 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
@@ -1,50 +1,53 @@
 #include "bits/stdc++.h"
 using namespace std;
-// 상 좌우, 하 좌우, 우 상하, 좌 상하
-int dx[12]={0,0,-1,1,-1,1,-1,1,2,2,-2,-2};
-int dy[12]={-1,1,0,0,-2,-2,2,2,-1,1,-1,1};
 
-struct P
-{
-    int y,x,Cnt,Rem;
-};
+int dy[]={-1,1,0,0,-2,-1,1,2,2,1,-1,-2};
+int dx[]={0,0,-1,1,1,2,2,1,-1,-2,-2,-1};
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
     
-    int K, W, H;
+    int K,W,H;
     cin>>K>>W>>H;
-    vector<vector<int>> Map(H,vector<int>(W,0));
-    vector<vector<vector<bool>>> Visited(H,vector<vector<bool>>(W,vector<bool>(K+1,false)));
-    for(auto& r : Map)
-        for(auto& c : r)
-            cin>>c;
     
-    queue<P> q;
-    q.push({0,0,0,K});
-    Visited[0][0][K]=true;
-    int Res=-1;
-    while(!q.empty())
+    vector<vector<int>> Map(H,vector<int>(W,0));
+    vector<vector<vector<int>>> Dist(H,vector<vector<int>>(W,vector<int>(K+1,1e9)));
+    for(auto& r : Map)
     {
-        int cy=q.front().y, cx=q.front().x, Cnt=q.front().Cnt, Rem=q.front().Rem;
-        q.pop();
-        if(cy==H-1 && cx==W-1)
+        for(auto& c : r)
         {
-            Res=Cnt;
-            break;
-        }
-        for(int i=0;i<12;++i)
-        {
-            if(Rem<=0 && i>3) break;
-            int ny=cy+dy[i], nx=cx+dx[i];
-            int NxtRem=Rem - (i>3 ? 1 : 0);
-            if(ny<0 || ny>=H || nx<0 || nx>=W || Visited[ny][nx][NxtRem] || Map[ny][nx]) continue;
-            
-            Visited[ny][nx][NxtRem]=true;
-            q.push({ny,nx,Cnt+1,NxtRem});
+            cin>>c;
         }
     }
-    cout<<Res;
+    queue<tuple<int,int,int>> Q;
+    Q.push({0,0,K});
+    Dist[0][0][K]=0;
+    bool bIsFound=false;
+    while(!Q.empty() && !bIsFound)
+    {
+        auto [Cy,Cx,Cnt]=Q.front();
+        Q.pop();
+        for(int i=0;i<12;++i)
+        {
+            int Ny=Cy+dy[i], Nx=Cx+dx[i], NxtCnt=Cnt;
+            if(Ny<0 || Ny>=H || Nx<0 || Nx>=W)              continue;
+            if(i>3)
+            {
+                if(Cnt<1) break;
+                else      --NxtCnt;
+            }
+            if(Map[Ny][Nx]==1 || Dist[Ny][Nx][NxtCnt]!=1e9) continue;
+            Dist[Ny][Nx][NxtCnt]=Dist[Cy][Cx][Cnt]+1;
+            if(Ny==H-1 && Nx==W-1) 
+            {
+                bIsFound=true;
+                break;
+            }
+            Q.push({Ny,Nx,NxtCnt});
+        }
+    }
+    int Ans=*min_element(Dist[H-1][W-1].begin(), Dist[H-1][W-1].end());
+    if(Ans==1e9) cout<<-1;
+    else         cout<<Ans;
 }
