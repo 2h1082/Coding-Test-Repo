4e9ee98bfbb18e8808895b00461af28f5b2a94e6
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-02-18 01:48:58 +0900

    [Platinum V] Title: 비숍, Time: 20 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Platinum/1799. 비숍/비숍.cc
+++ 백준/Platinum/1799. 비숍/비숍.cc
@@ -1,57 +1,51 @@
-#include <bits/stdc++.h>
-
+#include "bits/stdc++.h"
 using namespace std;
-vector<bool> Diag1,Diag2;
-vector<pair<int,int>> Black,White;
-int N=0, Max=0;
-bool Possible(int y, int x)
-{
-    return !(Diag1[y-x+N-1]||Diag2[y+x]);
-}
 
-void DFS(vector<pair<int,int>>& Cells,int Idx, int Cnt, int& Max)
+int N;
+vector<vector<int>> A;
+vector<bool> Diag1, Diag2;
+void DFS(int Cur, int Cnt, int& M, int& Max, vector<pair<int,int>>& Cand)
 {
-    if(Idx==Cells.size())
+    if(Cur==M)
     {
         Max=max(Max,Cnt);
         return;
     }
-    auto [y,x]=Cells[Idx];
-    if(Possible(y,x))
+    auto [Y,X]=Cand[Cur];
+    DFS(Cur+1,Cnt,M,Max,Cand);
+    if(!Diag1[Y-X+N] && !Diag2[Y+X]) 
     {
-        Diag1[y-x+N-1]=true;
-        Diag2[y+x]=true;
-        DFS(Cells,Idx+1,Cnt+1,Max);
-        Diag1[y-x+N-1]=false;
-        Diag2[y+x]=false;
+        Diag1[Y-X+N]=Diag2[Y+X]=true;
+        DFS(Cur+1,Cnt+1,M,Max,Cand);
+        Diag1[Y-X+N]=Diag2[Y+X]=false;
     }
-    DFS(Cells,Idx+1,Cnt,Max);
 }
-
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     
     cin>>N;
-    
+    A.assign(N,vector<int>(N,0));
     Diag1.assign(2*N,false);
     Diag2.assign(2*N,false);
-    for(int r=0;r<N;++r)
+    // 흰색, 흑색 칸은 비숍 배치에 서로 영향을 주지 않으니 아예 나누어 시뮬레이션 -> 시간 절약 가능
+    // - 불필요한 경우의 수를 계산할 필요가 없어짐
+    vector<pair<int,int>> B,W;
+    for(int i=0;i<N;++i)
     {
-        for(int c=0;c<N;++c)
+        for(int j=0;j<N;++j)
         {
-            int Num=0;
-            cin>>Num;
-            if(Num!=1) continue;
-            if((r+c)%2) White.push_back({r,c});
-            else        Black.push_back({r,c});
-            
+            cin>>A[i][j];
+            if(!A[i][j]) continue;
+            if((i+j)%2)  B.push_back({i,j});
+            else         W.push_back({i,j});
+                
         }
     }
-    int MaxB=0,MaxW=0;
-    DFS(Black,0,0,MaxB);
-    DFS(White,0,0,MaxW);
-    cout<<MaxB+MaxW;
+    int MaxB=0, MaxW=0, SizeB=B.size(), SizeW=W.size();
+    DFS(0,0,SizeB,MaxB,B);
+    DFS(0,0,SizeW,MaxW,W);
+    
+    cout<<MaxW+MaxB;
 }
