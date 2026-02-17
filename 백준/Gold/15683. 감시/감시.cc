a2ae96b28b58312a8d8c8cf58afac03b6366022d
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-02-18 02:21:25 +0900

    [Gold III] Title: 감시, Time: 8 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,84 +1,73 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int dx[4]={0,-1,0,1};
-int dy[4]={-1,0,1,0};
-vector<vector<vector<int>>> Dirs={
+int dy[]={-1,1,0,0};
+int dx[]={0,0,-1,1};
+// 카메라 타입별 가능한 방향 배치 세트
+vector<vector<vector<int>>> DirSet={
     {},
     {{0},{1},{2},{3}},
-    {{0,2},{1,3}},
-    {{0,1},{1,2},{2,3},{3,0}},
-    {{0,1,2},{1,2,3},{2,3,0},{3,0,1}},
+    {{0,1},{2,3}},
+    {{0,2},{0,3},{1,2},{1,3}},
+    {{0,1,2},{0,1,3},{0,2,3},{1,2,3}},
     {{0,1,2,3}}
 };
+int Total=0, N, M, Size=0, Max=0;
+vector<vector<int>> A;
 vector<pair<int,int>> Camera;
-vector<vector<int>> Map;
-int Max=0;
-int N,M;
-
-int Watch(int y, int x, int Dir, vector<pair<int,int>>& Changed)
+int Watch(int Y, int X, int D, vector<pair<int,int>>& Changed)
 {
-    int ny=y, nx=x, Cnt=0;
+    int Ny=Y, Nx=X, Cnt=0;
     while(1)
     {
-        ny+=dy[Dir];
-        nx+=dx[Dir];
-        if(ny<0 || ny>=N || nx<0 || nx>=M) break;
-        if(Map[ny][nx]==6)                 break;
-        
-        if(!Map[ny][nx])
+        Ny+=dy[D];
+        Nx+=dx[D];
+        if(Ny<0 || Ny>=N || Nx<0 || Nx>=M || A[Ny][Nx]==6) break;
+        if(!A[Ny][Nx])
         {
             ++Cnt;
-            Map[ny][nx]=-1;
-            Changed.push_back({ny,nx});
+            Changed.push_back({Ny,Nx});
+            A[Ny][Nx]=-1;
         }
     }
     return Cnt;
 }
-void DFS(int Idx, int Cnt)
+void DFS(int Cur, int Cnt)
 {
-    if(Idx==Camera.size())
+    if(Cur==Size)
     {
         Max=max(Max,Cnt);
         return;
     }
-    auto [y,x]=Camera[Idx];
-    int Type=Map[y][x];
-    for(auto& DirSet : Dirs[Type])
+    auto [Y,X]=Camera[Cur];
+    for(auto& Dir : DirSet[A[Y][X]])
     {
         vector<pair<int,int>> Changed;
         int Add=0;
-        for(int D : DirSet) Add+=Watch(y,x,D,Changed);
-        
-        DFS(Idx+1,Cnt+Add);
-        
-        for(auto [r,c] : Changed) Map[r][c]=0;
+        for(int D : Dir) Add+=Watch(Y,X,D,Changed);
+        DFS(Cur+1,Cnt+Add);
+        for(auto& [r,c] : Changed) A[r][c]=0;
     }
 }
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
     
     cin>>N>>M;
-    
-    Map.assign(N,vector<int>(M,0));
-    
-    int Total=N*M;
+    A.assign(N,vector<int>(M,0));
+    Total=N*M;
     for(int i=0;i<N;++i)
     {
         for(int j=0;j<M;++j)
         {
-            cin>>Map[i][j];
-            if(Map[i][j]) 
-            {
-                --Total;
-                if(Map[i][j]!=6) Camera.push_back({i,j});
-            }
+            cin>>A[i][j];
+            if(!A[i][j]) continue;
+            --Total;
+            if(A[i][j]!=6) Camera.push_back({i,j});
         }
     }
+    Size=Camera.size();
     DFS(0,0);
-    
     cout<<Total-Max;
 }
