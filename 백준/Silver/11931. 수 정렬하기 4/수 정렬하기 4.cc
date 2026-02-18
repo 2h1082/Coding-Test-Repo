4a235f2696ac5a7f5a98f921043c7cf0eb122bc0
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-02-19 07:44:16 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 248 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -1,16 +1,15 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cout.tie(NULL);
     cin.tie(NULL);
     
-    int N=0;
+    int N;
     cin>>N;
-    vector<int> Num(N,0);
-    for(auto& n : Num) cin>>n;
-    sort(Num.begin(),Num.end(),greater<int>());
-    for(auto& n : Num) cout<<n<<'\n';
+    vector<int> A(N);
+    for(auto& a : A) cin>>a;
+    sort(A.begin(),A.end(),greater<>());
+    for(auto& a : A) cout<<a<<'\n';
 }
