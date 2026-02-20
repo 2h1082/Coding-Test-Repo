21a75d6bb76e4fc8dab7144abb17e002eb2b2657
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-02-21 00:30:53 +0900

    [Silver V] Title: 단어 정렬, Time: 16 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,26 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include "bits/stdc++.h"
 using namespace std;
+
 int main()
 {
-    int N=0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N;
     cin>>N;
-    vector<string> Words(N);
-    for (int i=0;i<N;++i)
-    {
-        cin>>Words[i];
-    }
-    sort(Words.begin(),Words.end(),[&](const string& a, const string& b)
-    {
-        if (a.length()!=b.length()) return a.length()<b.length();
-        return a<b;
+    vector<string> A(N);
+    for(auto& a : A) cin>>a;
+    sort(A.begin(),A.end(),[](const auto& a, const auto& b){
+        if(a.length() != b.length()) return a.length() < b.length();
+        return a < b;
     });
-    auto NewEnd=unique(Words.begin(),Words.end());
-    Words.erase(NewEnd,Words.end());
-    for (auto& word:Words)
-    {
-        cout<<word<<endl;
-    }
+    A.erase(unique(A.begin(),A.end()),A.end());
+    for(auto& a : A) cout<<a<<'\n';
 }
