afa807cdeb5d60f01b4a4df6b507102a04329c40
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-02-24 05:35:49 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,27 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-
+#include "bits/stdc++.h"
 using namespace std;
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int N=0, M=0;
+    
+    int N,M;
     cin>>N>>M;
-    vector<int> V(N);
+    
     vector<int> Dp(N);
     for(int i=0;i<N;++i)
     {
-        cin>>V[i];
-        if(i>0) Dp[i]=Dp[i-1]+V[i];
-        else    Dp[i]=V[i];
+        cin>>Dp[i];
+        if(i>0) Dp[i]+=Dp[i-1];
     }
+    
     while(M--)
     {
-        int i=0,j=0;
-        cin>>i>>j;
-        int Answer= i==1 ? Dp[j-1] : Dp[j-1]-Dp[i-2];
-        cout<<Answer<<"\n";
+        int S,E;
+        cin>>S>>E;
+        
+        int Ans=Dp[E-1];
+        if(S>1) Ans-=Dp[S-2];
+        cout<<Ans<<'\n';
     }
 }
