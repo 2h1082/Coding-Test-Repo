8d5fabae37397f910eb9a97ba137f55e7645b1a2
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-02-25 18:08:07 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,44 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-#include <cmath>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
-   
-    int T=0;
+    
+    vector<vector<int>> Dp(41,vector<int>(2,0));
+    Dp[0][0]=Dp[1][1]=1;
+    for(int i=2;i<=40;++i)
+    {
+        Dp[i][0]=Dp[i-1][0]+Dp[i-2][0];
+        Dp[i][1]=Dp[i-1][1]+Dp[i-2][1];
+    }
+    int T;
     cin>>T;
-    vector<int> Q(T);
-    int Max=0;
-    for(int i=0;i<T;++i)
+    while(T--)
     {
-        cin>>Q[i];
-        Max=max(Q[i],Max);
-    }
-    vector<vector<int>> Dp(Max+1,vector<int>(2,0));
-    Dp[0][0]=1;
-    if(Max!=0) Dp[1][1]=1;
-    for(int i=2;i<=Max;++i)
-    {
-        Dp[i][0]=Dp[i-2][0]+Dp[i-1][0];
-        Dp[i][1]=Dp[i-2][1]+Dp[i-1][1];
-    }
-    for(auto& Test:Q)
-    {
-        cout<<Dp[Test][0]<<" "<<Dp[Test][1]<<"\n";
+        int N;
+        cin>>N;
+        cout<<Dp[N][0]<<' '<<Dp[N][1]<<'\n';
     }
 }
 /*
-    0  1
-0 - 1, 0
-1 - 0, 1
-2 - 1, 1
-3 - 1, 2
-4 - 2  3
-5 - 3  5
-6 - 5  8
+0 - 1 0
+1 - 0 1
+2 - 1 1
+3 - 1 2
+4 - 2 3
+5 - 3 5
+6 - 5 8
 */
