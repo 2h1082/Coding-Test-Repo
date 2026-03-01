72c5f01c5cf70c9f46ea51e5d076fa029a60eb4f
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-03-01 23:33:06 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,23 +1,22 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
-#define t first
-#define p second
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
     
-    int N=0;
+    int N;
     cin>>N;
-    vector<pair<int,int>> Info(N+1,{0,0});
-    for(int i=1;i<=N;++i) cin>>Info[i].t>>Info[i].p;
-    vector<int> DP(N+1,0);
-    for(int i=1;i<=N;++i)
+    
+    vector<pair<int,int>> A(N);
+    for(auto& [t,p]:A) cin>>t>>p;
+    vector<int> Dp(N+1,0);
+    for(int i=0;i<N;++i)
     {
-        DP[i]=max(DP[i],DP[i-1]);
-        int End=i+Info[i].t-1;
-        if(End<=N) DP[End]=max(DP[End],DP[i-1]+Info[i].p);
+        Dp[i+1]=max(Dp[i],Dp[i+1]);
+        int E=i+A[i].first;
+        if(E <= N) Dp[E]=max(Dp[E],Dp[i]+A[i].second);
     }
-    cout<<DP[N];
+    cout<<Dp[N];
 }
