5566681b8e89f99c0cbdf4adfe3b616bb3bd29d2
2h1082 <95967939+2h1082@users.noreply.github.com>
2026-03-01 22:10:26 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,34 +1,27 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include "bits/stdc++.h"
 using namespace std;
-int N=0;
-vector<int> Nums;
-int Max=0;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
     
+    int N;
     cin>>N;
-    Nums.assign(N,0);
+    vector<int> Ans;
     for(int i=0;i<N;++i)
     {
-        cin>>Nums[i];
-    }
-    vector<int> DP(N,1);
-    for(int i=0;i<N;++i)
-    {
-        for(int j=i+1;j<N;++j)
+        int Cur;
+        cin>>Cur;
+        if(!Ans.empty() && Ans.back() >= Cur)
         {
-            if(Nums[j]>Nums[i])
-            {
-                DP[j]=max(DP[j],DP[i]+1);
-            }
+            auto It=lower_bound(Ans.begin(),Ans.end(),Cur);
+            *It=Cur;
+        }
+        else
+        {
+            Ans.push_back(Cur);
         }
     }
-    Max=*max_element(DP.begin(),DP.end());
-    cout<<Max;
+    cout<<Ans.size();
 }
